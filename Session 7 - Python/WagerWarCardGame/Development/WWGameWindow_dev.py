ed3194e81a7f43e5025ad6584fce498fbe28ba1f
dbowmans46 <dbowmans46@gmail.com>
2019-08-07 12:48:12 -0400

    Added development version of codebase

---
+++ Session 7 - Python/WagerWarCardGame/Development/WWGameWindow_dev.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+LICENSE (MIT License):
+
+Copyright 2018 Jason Gilbert, Ryan Concienne, and Douglas Bowman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+"""
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QLineEdit, QPushButton, QComboBox, QDialog
+
+class WWGameWindow(object):
+    
+    def setupUi(self, wwGameManager):
+
+        self.wwgm = wwGameManager
+        self.MainWindow = QDialog()
+               
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("background-color: rgb(0, 85, 0);")
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(40, 20, 711, 531))
+        self.widget.setObjectName("widget")
+
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+
+        self.playerDeckMain = QtWidgets.QLabel(self.widget)
+        self.playerDeckMain.setObjectName("playerDeckMain")
+
+        self.gridLayout.addWidget(self.playerDeckMain, 11, 2, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+
+        self.computerNameLabel = QtWidgets.QLabel(self.widget)
+        self.computerNameLabel.setObjectName("computerNameLabel")
+        
+        self.gridLayout.addWidget(self.computerNameLabel, 4, 3, 1, 1)
+        
+        self.warTitle = QtWidgets.QLabel(self.widget)
+        self.warTitle.setStyleSheet("background-image: url(:/Main/q02th.jpg);")
+        self.warTitle.setObjectName("warTitle")
+        
+        self.gridLayout.addWidget(self.warTitle, 2, 3, 1, 1)
+        
+        self.playerDeckDiscard = QtWidgets.QLabel(self.widget)
+        self.playerDeckDiscard.setObjectName("playerDeckDiscard")
+        
+        self.gridLayout.addWidget(self.playerDeckDiscard, 11, 4, 1, 1, QtCore.Qt.AlignHCenter)
+        
+        self.compDeckDiscard = QtWidgets.QLabel(self.widget)
+        self.compDeckDiscard.setObjectName("compDeckDiscard")
+        
+        self.gridLayout.addWidget(self.compDeckDiscard, 10, 4, 1, 1, QtCore.Qt.AlignHCenter)
+        
+        self.compDeckActive = QtWidgets.QLabel(self.widget)
+        self.compDeckActive.setObjectName("compDeckActive")
+        
+        self.gridLayout.addWidget(self.compDeckActive, 10, 3, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        
+        self.palyerNameLabel = QtWidgets.QLabel(self.widget)
+        self.palyerNameLabel.setObjectName("palyerNameLabel")
+        
+        self.verticalLayout.addWidget(self.palyerNameLabel)
+        
+        self.gridLayout.addLayout(self.verticalLayout, 14, 3, 1, 1)
+        
+        self.compDeckMain = QtWidgets.QLabel(self.widget)
+        self.compDeckMain.setObjectName("compDeckMain")
+        
+        self.gridLayout.addWidget(self.compDeckMain, 10, 2, 1, 1, QtCore.Qt.AlignHCenter)
+        
+        self.autoCompletPushButton = QtWidgets.QPushButton(self.widget)
+        self.autoCompletPushButton.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(138, 138, 138);")
+        self.autoCompletPushButton.setObjectName("autoCompletPushButton")
+        
+        self.gridLayout.addWidget(self.autoCompletPushButton, 16, 2, 1, 1)
+        
+        self.playerDeckActive = QtWidgets.QLabel(self.widget)
+        self.playerDeckActive.setObjectName("playerDeckActive")
+        
+        self.gridLayout.addWidget(self.playerDeckActive, 11, 3, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        
+        self.DesignedByLabel = QtWidgets.QLabel(self.widget)
+        self.DesignedByLabel.setObjectName("DesignedByLabel")
+        
+        self.gridLayout.addWidget(self.DesignedByLabel, 18, 1, 1, 6, QtCore.Qt.AlignHCenter)
+        
+        self.quitPushButton = QtWidgets.QPushButton(self.widget)
+        self.quitPushButton.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(138, 138, 138);")
+        self.quitPushButton.setObjectName("quitPushButton")
+        
+        self.gridLayout.addWidget(self.quitPushButton, 16, 4, 1, 1, QtCore.Qt.AlignHCenter)
+        
+        self.dealPushButton = QtWidgets.QPushButton(self.widget)
+        self.dealPushButton.setStyleSheet("color: rgb(255, 255, 255); background-color: rgb(138, 138, 138);")
+        self.dealPushButton.setObjectName("dealPushButton")
+        
+        self.gridLayout.addWidget(self.dealPushButton, 16, 3, 1, 1, QtCore.Qt.AlignHCenter)
+        
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        
+        self.gridLayout.addLayout(self.verticalLayout_3, 10, 1, 1, 1)
+        
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        
+        self.gridLayout.addLayout(self.verticalLayout_2, 2, 1, 1, 1)
+        
+        self.cardCountCompLabel = QtWidgets.QLabel(self.widget)
+        self.cardCountCompLabel.setObjectName("cardCountCompLabel")
+        
+        self.gridLayout.addWidget(self.cardCountCompLabel, 5, 3, 1, 1)
+        
+        self.cardCountPlayerLabel = QtWidgets.QLabel(self.widget)
+        self.cardCountPlayerLabel.setObjectName("cardCountPlayerLabel")
+        
+        self.gridLayout.addWidget(self.cardCountPlayerLabel, 15, 3, 1, 1)
+        
+        self.autoCompletPushButton.raise_()
+        
+        self.dealPushButton.raise_()
+        
+        self.warTitle.raise_()
+        
+        self.playerDeckDiscard.raise_()
+        
+        self.DesignedByLabel.raise_()
+        
+        self.compDeckActive.raise_()
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+        
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(self.MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(self.MainWindow)
+
+        self.MainWindow.show()
+
+        self.WagerWarGameMan = wwGameManager
+
+        self.dealPushButton.clicked.connect(self.turnEventUpdate)
+
+        """
+        @brief Update GUI: update Player/Comp Card Count, Turn Count, Populate the new card image.
+        @param
+        """
+        def turnEventUpdate(self):
+
+            return None
+
+        def pullSetup():
+            print("")
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        
+        self.playerDeckMain.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.computerNameLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffffff;\">&lt;comp_name&gt;</span></p></body></html>"))
+        
+        self.warTitle.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:20pt; color:#ffffff;\">War!!!</span></p></body></html>"))
+        
+        self.playerDeckDiscard.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.compDeckDiscard.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.compDeckActive.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.palyerNameLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffffff;\">&lt;player_name&gt;</span></p></body></html>"))
+        
+        self.compDeckMain.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.autoCompletPushButton.setText(_translate("MainWindow", "Auto Complete"))
+        
+        self.playerDeckActive.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Main/production/blueBackVert.bmp\"/></p></body></html>"))
+        
+        self.DesignedByLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; font-style:italic; color:#ffffff;\">Designed By: Peoples\'</span></p></body></html>"))
+        
+        self.quitPushButton.setText(_translate("MainWindow", "Quit"))
+        
+        self.dealPushButton.setText(_translate("MainWindow", "Deal"))
+        
+        self.cardCountCompLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffffff;\">Card Count: + &lt;int&gt;</span></p></body></html>"))
+        
+        self.cardCountPlayerLabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:14pt; color:#ffffff;\">Card Count: + &lt;int&gt;</span></p></body></html>"))
---
+++ Session 7 - Python/WagerWarCardGame/Development/WWWarConstants.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+"""
+LICENSE (MIT License):
+
+Copyright 2018 Jason Gilbert, Ryan Concienne, and Douglas Bowman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to 
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+SOFTWARE.
+"""
+import sys
+from PyQt5 import QtWidgets, QtCore
+#from win32api import GetSystemMetrics
+
+"""
+@brief This is a static class
+"""
+class WWWarConstants():
+    
+    """
+    Set standard size of deck
+    """
+    DECK_SIZE = 52
+    
+    """
+    File location of cards
+    """
+    CARD_PICTURE_LOCATION = "./resources/production/"
+    
+    # Card name will dictate value--change file picture accordingly
+    CARD_FILE_NAMES =  ['c01.bmp',
+                        'c02.bmp',
+                        'c03.bmp',
+                        'c04.bmp',
+                        'c05.bmp',
+                        'c06.bmp',
+                        'c07.bmp',
+                        'c08.bmp',
+                        'c09.bmp',
+                        'c10.bmp',
+                        'c11.bmp',
+                        'c12.bmp',
+                        'c13.bmp',
+                        'd01.bmp',
+                        'd02.bmp',
+                        'd03.bmp',
+                        'd04.bmp',
+                        'd05.bmp',
+                        'd06.bmp',
+                        'd07.bmp',
+                        'd08.bmp',
+                        'd09.bmp',
+                        'd10.bmp',
+                        'd11.bmp',
+                        'd12.bmp',
+                        'd13.bmp',
+                        'h01.bmp',
+                        'h02.bmp',
+                        'h03.bmp',
+                        'h04.bmp',
+                        'h05.bmp',
+                        'h06.bmp',
+                        'h07.bmp',
+                        'h08.bmp',
+                        'h09.bmp',
+                        'h10.bmp',
+                        'h11.bmp',
+                        'h12.bmp',
+                        'h13.bmp',
+                        's01.bmp',
+                        's02.bmp',
+                        's03.bmp',
+                        's04.bmp',
+                        's05.bmp',
+                        's06.bmp',
+                        's07.bmp',
+                        's08.bmp',
+                        's09.bmp',
+                        's10.bmp',
+                        's11.bmp',
+                        's12.bmp',
+                        's13.bmp']
+    
+    """
+    @brief Card style Dictionary 
+    @parma key, value
+    """
+    deckStyle = {"BLUE":"blueBackVert.bmp",
+                 "RED":"redBackVert.bmp",
+                 "CAMO":"camo.bmp"}
+    
+    
+    """
+    @ brief string used on all GUI windows, A credit to the developers
+    """
+    #COULD NOT GET TO WORK ON 02-06-2019
+#    DesignedByLabel = QtWidgets.QLabel("Designed By Peoples:")
+#    DesignedByLabel.setGeometry(QtCore.QRect(430, 120, 341, 58))
+#    DesignedByLabel.setFont ("font-size:14pt; font-style:italic; color:#ffffff;")
+    
+        
+    """
+    @ brief Retrieve users resolution to use for window size
+    """
+    
+#    ResWidth = GetSystemMetrics(0)
+#    ResHeight = GetSystemMetrics(1)
+    
+    

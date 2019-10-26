c14bd25ac3559448593b6e307b99fa2b2f45aa7c
Doug B <dbowmans46@gmail.com>
2019-10-26 11:05:35 -0400

    Added updated review powerpoint, class example code, and the start of a mini project

---
+++ Session 7 - Python/Instructional Material/Class Examples/In-Class Meanderings 20191025.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 11:40:15 2019
+
+@author: Doug
+"""
+huh = 5
+string1 = 'Erin rocks'
+string2 = "erin rocks"
+
+myboo = True
+
+float(huh)
+
+dictionary = {'first':'1st',
+              'second':'2nd'}
+
+
+foodsList = ['beer','wine','booze','cheese','meats']
+
+

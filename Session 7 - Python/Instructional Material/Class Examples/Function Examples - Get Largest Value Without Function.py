b28b9d1d517535e514c9534c31a8cb5bed52d3bb
Doug B <dbowmans46@gmail.com>
2019-11-07 13:09:00 -0500

    Added example for importance of functions, added slides for functions in powerpoint.

---
+++ Session 7 - Python/Instructional Material/Class Examples/Function Examples - Get Largest Value Without Function.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 13:03:05 2019
+
+@author: Doug
+"""
+
+listVals = [10,3,6,8,2,6]
+maxValue = None
+
+for val in listVals:
+    
+    if maxValue == None:
+        maxValue = val
+    elif val > maxValue:
+        maxValue=val
+        
+print(maxValue)
+    
+    
+listVals = [2,34,789,4456,2,555,-12]
+
+listVals = [-12,-4,-2,-7,-17]
+
+listVals = [3,-1,45,12,9]
+
+listVals = [-3,57,90,-10000,12,0.0001]
+
+listVals = [0.0001,0.0003,0.0008283,0.00000000483]
+

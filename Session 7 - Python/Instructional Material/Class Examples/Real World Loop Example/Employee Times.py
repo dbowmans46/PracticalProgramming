5ea3d38771ec288431d47c002c946d8209a0d52e
Doug B <dbowmans46@gmail.com>
2019-11-12 08:02:07 -0500

    Added real-world example problems for loops, updated powerpoint slides

---
+++ Session 7 - Python/Instructional Material/Class Examples/Real World Loop Example/Employee Times.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 07:51:01 2019
+
+Copyright 2019 Douglas Bowman dbowmans46@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+# Employee name is the key, time worked is the value
+employeeTimes = {
+        'Bob' : ,
+        'Sally': ,
+        'Reinholdt' : ,
+        'Jack Ryan' : ,
+        'Joan' : ,
+        'Weatherby' : ,
+        'Scilia' : ,
+        'Sally': ,
+        'Jack Ryan' : ,
+        'Weatherby' : ,
+        'Joan' : ,
+        'Joan' : ,
+        'Sally': ,
+        'Reinholdt' : ,
+        'Bob' : ,
+        'Reinholdt' : ,
+        'Joan' : ,
+        'Jack Ryan' : ,
+        'Sally': ,
+        'Bob' : ,
+        'Sally': ,
+        'Reinholdt' : ,
+        'Reinholdt' : ,
+        'Joan' : 
+        }
+    
+    
+# TODO: Create variable to hold times
+    
+# TODO: Loop through each key-value pair of the employeeTimes dictionary
+    
+# TODO: Add each time to the appropriate person
+    
+# TODO: Manually add the times of one employee to test script functionality
---
+++ Session 7 - Python/Instructional Material/Class Examples/Real World Loop Example/Parts in Repair.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 07:57:15 2019
+
+Copyright 2019 Douglas Bowman dbowmans46@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+# part is the key, need-by time is the value
+partNeedByTimes = {
+        
+        }

9e8289cf54032ec77313bdbac38e346273751e2b
Doug B <dbowmans46@gmail.com>
2020-03-06 00:29:24 -0500

    Updated slides, added data, added review script

---
+++ Session 7 - Python/Instructional Material/Class Examples/Review Examples.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 00:13:31 2020
+
+@author: Doug
+"""
+
+# TODO: basic variables
+# TODO: tuple
+# TODO: list
+# TODO: dictionary
+# TODO: Conditions
+# TODO: Loops
+# TODO: Functions
+# TODO: Write to a file that doesn't exist
+# TODO: Read from a file
+# TODO: Append data to a file

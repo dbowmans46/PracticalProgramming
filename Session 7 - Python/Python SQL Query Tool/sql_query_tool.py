cdc6fca4046c4a681ab00888fe88fe22df76d706
Doug B <dbowmans46@gmail.com>
2020-09-24 19:53:33 -0400

    Rough layout of sql query tool project

---
+++ Session 7 - Python/Python SQL Query Tool/sql_query_tool.py
@@ -0,0 +1,23 @@
+# TODO: 
+
+# TODO: Database Connection (use compound datatype to house all connections)
+
+# TODO: Write SQL query, test using Excel
+
+# TODO: Load SQL query from external file
+
+# TODO: Get data into pandas DataFrame
+
+# TODO: Write data to .csv file
+
+# TODO: Write data to Excel file
+
+# TODO: Create conditional to either output data as .csv or Excel file depending on variable
+
+# TODO: Optional: function for database connections
+
+# TODO: Optional: Class for database connections
+
+# TODO: Optional: Encrypt password as cypher text requireing key file
+
+# TODO: GUI using classes

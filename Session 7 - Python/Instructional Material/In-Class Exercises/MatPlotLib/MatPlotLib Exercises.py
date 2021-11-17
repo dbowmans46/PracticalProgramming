92be67ab10a88083071de09eb85b28eae9c83a3d
Douglas Bowman <dbowmans46@gmail.com>
2021-11-16 23:07:55 -0500

    Added in-class problems and workable data for use in problems.

---
+++ Session 7 - Python/Instructional Material/In-Class Exercises/MatPlotLib/MatPlotLib Exercises.py
@@ -0,0 +1,40 @@
+"""
+Instructions: Complete each TODO below.  The TODO's are put into sections for separate graphs.  Each section
+can be done with different data if desired, and if the data allows the questin to be answered.  Filtering should
+be done, if needed.  Example data can be found at ./Data/.
+
+License:
+Copyright 2021 Douglas Bowman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+# For the following TODO's, use a data set with a single x-y series
+# Workable data set: Any
+# TODO: Plot basic data using an appropriate plot type.
+# TODO: Add an appropriate x-label, y-label, and title.
+# TODO: Turn the grid on.
+# TODO: Change the x-axis limits and y-axis limits to more fully show the data (i.e. change the limits to as much data can be seen on the plot as possible).
+# TODO: Change the font sizes of the x-label, y-label, and title to be visible.  Make the title fontsize bigger than the labels.
+# TODO: Output the graph to a file.
+
+# For the following TODO's, multiple x-y series will be needed to plot on the same graph
+# Workable data set: NYC_Expense_Actuals.csv, Inflation 1980-2020.csv, 2020_earthquakes_4_5_plus.csv
+# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series has a different color, as well as different markers.  Different line styles may be used in place of markers as long as each series is distinguishable in black and white.
+# TODO: Add a legend.  Put it in an opportune place to minimize covering of data.
+# TODO: Add an appropriate x-label, y-label, and title.  
+# TODO: Add an annotation to depict an abnormal event in the data.
+# TODO: 
+
+# For the following TODO's, put each graph on it's own subplot.
+# Workable data set: NYC_Expense_Actuals.csv, Inflation 1980-2020.csv, 2020_earthquakes_4_5_plus.csv
+# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series is on its own plot in the subplot, and uses a different type of plot. Hint: think about plotting metadata, such as number of agencies per year, or how much portion of all funds the city fund has per year).
+# TODO: Add an appropriate x-label, y-label, and title for each subplot.
+# TODO: Reset x and y limits, as well as size and spacing, to get the data to show as best as possible.  If multiple subplots would work better, feel free to create them.
+# TODO: Adjust tick sizes and tick rotation as necessary to show data.
+# TODO: Print the plot to a file.

7266351f678ace622925c8956f11236a4d76fca1
Doug Bowman <dbowmans46@gmail.com>
2021-11-25 10:58:44 -0500

    Formatted for clarity

--- Session 7 - Python/Instructional Material/In-Class Exercises/MatPlotLib/MatPlotLib Exercises.py
+++ Session 7 - Python/Instructional Material/In-Class Exercises/MatPlotLib/MatPlotLib Exercises.py
@@ -19,22 +19,46 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 # TODO: Plot basic data using an appropriate plot type.
 # TODO: Add an appropriate x-label, y-label, and title.
 # TODO: Turn the grid on.
-# TODO: Change the x-axis limits and y-axis limits to more fully show the data (i.e. change the limits to as much data can be seen on the plot as possible).
-# TODO: Change the font sizes of the x-label, y-label, and title to be visible.  Make the title fontsize bigger than the labels.
+# TODO: Change the x-axis limits and y-axis limits to more fully show the data 
+#       (i.e. change the limits to as much data can be seen on the plot as 
+#       possible).
+# TODO: Change the font sizes of the x-label, y-label, and title to be visible.  
+#       Make the title fontsize bigger than the labels.
 # TODO: Output the graph to a file.
 
-# For the following TODO's, multiple x-y series will be needed to plot on the same graph
-# Workable data set: NYC_Expense_Actuals.csv, Inflation 1980-2020.csv, 2020_earthquakes_4_5_plus.csv
-# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series has a different color, as well as different markers.  Different line styles may be used in place of markers as long as each series is distinguishable in black and white.
-# TODO: Add a legend.  Put it in an opportune place to minimize covering of data.
+
+
+# For the following TODO's, multiple x-y series will be needed to plot on the 
+# same graph.
+# Workable data sets: 
+#     NYC_Expense_Actuals.csv
+#     Inflation 1980-2020.csv
+#     2020_earthquakes_4_5_plus.csv
+#
+# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series 
+#       has a different color, as well as different markers.  Different line  
+#       styles may be used in place of markers as long as each series is 
+#       distinguishable in black and white.
+# TODO: Add a legend.  Put it in an opportune place to minimize covering of 
+#       data.
 # TODO: Add an appropriate x-label, y-label, and title.  
 # TODO: Add an annotation to depict an abnormal event in the data.
-# TODO: 
+
+    
 
 # For the following TODO's, put each graph on it's own subplot.
-# Workable data set: NYC_Expense_Actuals.csv, Inflation 1980-2020.csv, 2020_earthquakes_4_5_plus.csv
-# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series is on its own plot in the subplot, and uses a different type of plot. Hint: think about plotting metadata, such as number of agencies per year, or how much portion of all funds the city fund has per year).
+# Workable data set: 
+#     NYC_Expense_Actuals.csv
+#     Inflation 1980-2020.csv
+#     2020_earthquakes_4_5_plus.csv
+#
+# TODO: Plot 5 series using an appropriate plot type.  Ensure each data series 
+#       is on its own plot in the subplot, and uses a different type of plot. 
+#       Hint: think about plotting metadata, such as number of agencies per 
+#       year, or how much portion of all funds the city fund has per year).
 # TODO: Add an appropriate x-label, y-label, and title for each subplot.
-# TODO: Reset x and y limits, as well as size and spacing, to get the data to show as best as possible.  If multiple subplots would work better, feel free to create them.
+# TODO: Reset x and y limits, as well as size and spacing, to get the data to 
+#       show as best as possible.  If multiple subplots would work better, feel 
+#       free to create them.
 # TODO: Adjust tick sizes and tick rotation as necessary to show data.
 # TODO: Print the plot to a file.

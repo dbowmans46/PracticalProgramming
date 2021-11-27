863f9e2d832e720fd24655b5900e67c8c259125f
Doug Bowman <dbowmans46@gmail.com>
2021-11-26 20:44:24 -0500

    Classifiers code, example on graphviz output for decision tree.

--- Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
+++ Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
@@ -52,6 +52,8 @@ data_train, data_test, target_train, target_test = \
                                              iris_dataset['target'], 
                                              random_state=0)
 
+print("\nIntro Model - K-Nearest Neighbors Classifier\n")
+
 # We can look at our data to see how it was split
 print("data_train.shape:", data_train.shape)
 print("data_test.shape:", data_test.shape)
@@ -63,9 +65,131 @@ from sklearn.neighbors import KNeighborsClassifier
 knn_model = KNeighborsClassifier(n_neighbors=1)  # Set the classifier type
 knn_model.fit(data_train, target_train)          # Train the model with data
 
-# We have now built the model, stored in teh knn_classifier variable.  But how
+# We have now built the model, stored in the knn_classifier variable.  But how
 # do we know if it is accurate?  Let us check this now by creating a prediction
 # from our test data, and seeing how well it matches our test targets.
 target_predictions = knn_model.predict(data_test)
 print("predictions:", target_predictions)
 print("Accuracy Score:", str(knn_model.score(data_test,target_test)*100) + "%")
+
+
+
+
+###############################################################################
+#                                                                             #
+#                    K-Nearest Neighbors Classifier                           #
+#                                                                             #
+###############################################################################
+
+print("\n\n\n\nK-Nearest Neighbors Classifier\n")
+
+# How does accuracy change with the number of neighbors?
+for neighbors in range(1,11):
+    knn_model = KNeighborsClassifier(n_neighbors=neighbors) 
+    knn_model.fit(data_train, target_train)         
+    target_predictions = knn_model.predict(data_test)
+    knn_score = round(knn_model.score(data_test,target_test)*100,8)
+    print("K-Nearest Neighbors accuracy neighbors=" + str(neighbors) + " score:", str(knn_score) + "%")
+
+
+print("\n\n\n\n")
+
+
+###############################################################################
+#                                                                             #
+#                       Decision Tree Classifier                              #
+#                                                                             #
+###############################################################################
+
+print("\nDecision Tree Classifier\n")
+
+
+# This example will use the decision tree classifier
+from sklearn.tree import DecisionTreeClassifier
+dec_tree_model = DecisionTreeClassifier(max_depth=3, random_state=0)  # Set the classifier type
+dec_tree_model.fit(data_train, target_train)            # Train the model with data
+dec_target_predictions = dec_tree_model.predict(data_test)
+print("Decision tree accuracy score:", str(dec_tree_model.score(data_test,target_test)*100) + "%")
+
+# Viewing the decision tree
+# Create a document that represents the tree
+from sklearn.tree import export_graphviz
+tree_file_name = "decision_tree.dot"
+class_name_vals = iris_dataset.target_names
+export_graphviz(dec_tree_model,out_file=tree_file_name, \
+                class_names=iris_dataset.target_names, \
+                feature_names=iris_dataset.feature_names, \
+                impurity=False, filled=True)
+    
+# Load the document and view
+import graphviz
+with open(tree_file_name) as fileHandle:
+    dot_graph = fileHandle.read()
+
+graphviz.Source(dot_graph)
+# If this does not work, you can always run dot.exe from the Graphviz
+# installation, and generate the graph manually.
+
+
+# How does accuracy change with the number of decisions?
+for max_depth_val in range(1,11):
+    dtm = DecisionTreeClassifier(max_depth=max_depth_val, random_state=0)  # Set the classifier type
+    dtm.fit(data_train, target_train)            # Train the model with data
+    dtm_target_predictions = dtm.predict(data_test)
+    dtm_score = round(dtm.score(data_test,target_test)*100,8)
+    print("Decision tree accuracy max_depth=" + str(max_depth_val) + " score:", str(dtm_score) + "%")
+
+
+# Why does the accuracy not improve after so many depth levels?  Hint: How many
+# attributes do we have?
+
+
+###############################################################################
+#                                                                             #
+#                     K-Nearest Neighbors Regressor                           #
+#                                                                             #
+###############################################################################
+
+
+
+###############################################################################
+#                                                                             #
+#                        Decision Tree Regressor                              #
+#                                                                             #
+###############################################################################
+
+
+
+###############################################################################
+#                                                                             #
+#                       Linear Regression Regressor                           #
+#                                                                             #
+###############################################################################
+
+
+###############################################################################
+#                                                                             #
+#                        Ridge Regression Regressor                           #
+#                                                                             #
+###############################################################################
+
+
+###############################################################################
+#                                                                             #
+#                        Lasso Regression Regressor                           #
+#                                                                             #
+###############################################################################
+
+
+###############################################################################
+#                                                                             #
+#                       Linear Regression Regressor                           #
+#                                                                             #
+###############################################################################
+
+
+
+
+
+
+

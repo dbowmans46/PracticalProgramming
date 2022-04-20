499542813440547d022c674221fc62e880e476ac
dbowmans46@gmail.com <dbowmans46@gmail.com>
2022-04-20 01:02:05 -0400

    Pipline example for data scaling, moved .pptx to .odp due to move to Debian (.pptx size > 100 MB GitHub limit), clarified interquartile range.

--- Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
+++ Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
@@ -691,51 +691,48 @@ def GetTrainTestSplitCovertypeData():
 #                                                                             #
 ###############################################################################
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
-from sklearn.datasets import load_iris, load_boston
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import cross_val_score
+# from sklearn.datasets import load_iris, load_boston
 
-print("Cross Validation on Regressor")
-print("------------------------------\n")
-data = load_iris()
+# print("Cross Validation on Regressor")
+# print("------------------------------\n")
+# data = load_iris()
 
-cv_model = LogisticRegression(max_iter=500000)
-cvs = cross_val_score(cv_model, data["data"], data["target"], scoring="accuracy")
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
+# cv_model = LogisticRegression(max_iter=500000)
+# cvs = cross_val_score(cv_model, data["data"], data["target"], scoring="accuracy")
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
-# The default number of folds is 5.  Can change this with the cv parameter
-print("\n\n\n")
-print("10 folds")
-cvs = cross_val_score(cv_model, data["data"], data["target"], scoring="accuracy", cv=10)
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
+# # The default number of folds is 5.  Can change this with the cv parameter
+# print("\n\n\n")
+# print("10 folds")
+# cvs = cross_val_score(cv_model, data["data"], data["target"], scoring="accuracy", cv=10)
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
+# # On regressors, we need to use a different scoring mechanism
+# print("\n\n\n")
+# print("Cross Validation on Regressor")
+# print("------------------------------\n")
+# data = load_boston()
 
+# l_mod = LinearRegression()
+# cvs = cross_val_score(l_mod, data["data"], data["target"], scoring="r2")
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
-
-# On regressors, we need to use a different scoring mechanism
-print("\n\n\n")
-print("Cross Validation on Regressor")
-print("------------------------------\n")
-data = load_boston()
-
-l_mod = LinearRegression()
-cvs = cross_val_score(l_mod, data["data"], data["target"], scoring="r2")
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
-
-# The default number of folds is 5.  Can change this with the cv parameter
-print("\n\n\n")
-print("10 folds")
-cvs = cross_val_score(l_mod, data["data"], data["target"], scoring="r2", cv=10)
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
+# # The default number of folds is 5.  Can change this with the cv parameter
+# print("\n\n\n")
+# print("10 folds")
+# cvs = cross_val_score(l_mod, data["data"], data["target"], scoring="r2", cv=10)
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
 
 
@@ -745,46 +742,66 @@ print("Cross Validation Standard Deviation: ", cvs.std())
 #                                                                             #
 ###############################################################################
 
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 
-# data_train, data_test, target_train, target_test = GetTrainTestSplitIrisData()
+data_train, data_test, target_train, target_test = GetTrainTestSplitIrisData()
 
-# C_val = 100,10
-# max_iterations = 100000
+C_val = 1000
+max_iterations = 100000
 
-# print("Accuracy without scaling:\n","--------------------\n")
-# log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
-# MLHelper.FitAndGetAccuracy(log_reg_model, data_train, data_test, \
-#                             target_train, target_test, 8)
+print("Accuracy without scaling:\n","--------------------\n")
+log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
+MLHelper.FitAndGetAccuracy(log_reg_model, data_train, data_test, \
+                            target_train, target_test, 8)
     
 
-# print("\n\n\n")
-# print("Accuracy with scaling:\n","--------------------\n")
+print("\n\n\n")
+print("Accuracy with scaling:\n","--------------------\n")
 
-# scaler = StandardScaler()
-# # scaler = RobustScaler()
-# # scaler = MinMaxScaler()
-# scaled_train_data = scaler.fit_transform(data_train, target_train)
-# scaled_test_data = scaler.fit_transform(data_test, target_test)
-# log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
-# MLHelper.FitAndGetAccuracy(log_reg_model, scaled_train_data, scaled_test_data, \
-#                             target_train, target_test, 8)
+scaler = StandardScaler()
+# scaler = RobustScaler()
+# scaler = MinMaxScaler()
+scaled_train_data = scaler.fit_transform(data_train, target_train)
+scaled_test_data = scaler.fit_transform(data_test, target_test)
+log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
+MLHelper.FitAndGetAccuracy(log_reg_model, scaled_train_data, scaled_test_data, \
+                            target_train, target_test, 8)
     
-# print("\n\n\n")
-# print("Accuracy with scaling and cross validation:\n","--------------------\n")
+print("\n\n\n")
+print("Accuracy with scaling and cross validation:\n","--------------------\n")
 
-# from sklearn.datasets import load_iris
-# from sklearn.model_selection import cross_val_score
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
 
-# data = load_iris()
-# scaler = StandardScaler()
-# scaled_data = scaler.fit_transform(data['data'], data['target'])
-# cvs = cross_val_score(log_reg_model, scaled_data, data["target"], scoring="accuracy")
+data = load_iris()
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(data['data'], data['target'])
+cvs = cross_val_score(log_reg_model, scaled_data, data["target"], scoring="accuracy")
     
-# print("Cross Validation Scores: ", cvs)
-# print("Cross Validation Mean: ", cvs.mean())
-# print("Cross Validation Standard Deviation: ", cvs.std())
+print("Cross Validation Scores: ", cvs)
+print("Cross Validation Mean: ", cvs.mean())
+print("Cross Validation Standard Deviation: ", cvs.std())
+
+
+# Scaling using a Pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
+print("\n\n\n")
+print("Accuracy with Scaling and Using Pipelines:")
+
+pipeline_w_scaler = Pipeline([
+    ("scalar", StandardScaler()),
+    ("log_reg", LogisticRegression(C=C_val, max_iter=max_iterations))
+    ])
+MLHelper.FitAndGetAccuracy(pipeline_w_scaler, data_train, data_test, \
+                            target_train, target_test, 8)
+cvs = cross_val_score(pipeline_w_scaler, data["data"], data["target"], scoring="accuracy")
+print("Cross Validation Scores: ", cvs)
+print("Cross Validation Mean: ", cvs.mean())
+print("Cross Validation Standard Deviation: ", cvs.std())
+
+
     
 ###############################################################################
 #                                                                             #

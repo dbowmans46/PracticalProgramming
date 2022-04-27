3ec7bbea5a9eabb6c95ed92c8d89a767f834818f
dbowmans46@gmail.com <dbowmans46@gmail.com>
2022-04-27 00:15:25 -0400

    String category encoding slides and example code added.  Started slides on adding missing data.

--- Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
+++ Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
@@ -243,6 +243,37 @@ def GetTrainTestSplitCovertypeData():
     return data_train, data_test, target_train, target_test
 
 
+def GetHousingData():    
+    """
+    Regression data set from https://github.com/ageron/handson-ml2/blob/master/datasets/housing/housing.csv
+
+    Returns
+    -------
+    data_train : TYPE
+        DESCRIPTION.
+    data_test : TYPE
+        DESCRIPTION.
+    target_train : TYPE
+        DESCRIPTION.
+    target_test : TYPE
+        DESCRIPTION.
+
+    """
+    
+    import pandas as pd
+    
+    data_file_path = "../../In-Class Exercises/Data/housing.csv"
+    data = pd.read_csv(data_file_path)
+    data_train, data_test, target_train, target_test = \
+        sklearn.model_selection.train_test_split(data.drop('median_house_value',axis=1), 
+                                              data['median_house_value'],
+                                              test_size=0.25,
+                                              train_size=0.75,
+                                              random_state=0)
+        
+    return data_train, data_test, target_train, target_test
+
+
 
 # print("\nIntro Model - K-Nearest Neighbors Classifier\n")
 
@@ -742,64 +773,64 @@ def GetTrainTestSplitCovertypeData():
 #                                                                             #
 ###############################################################################
 
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
 
-data_train, data_test, target_train, target_test = GetTrainTestSplitIrisData()
+# data_train, data_test, target_train, target_test = GetTrainTestSplitIrisData()
 
-C_val = 1000
-max_iterations = 100000
+# C_val = 1000
+# max_iterations = 100000
 
-print("Accuracy without scaling:\n","--------------------\n")
-log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
-MLHelper.FitAndGetAccuracy(log_reg_model, data_train, data_test, \
-                            target_train, target_test, 8)
+# print("Accuracy without scaling:\n","--------------------\n")
+# log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
+# MLHelper.FitAndGetAccuracy(log_reg_model, data_train, data_test, \
+#                             target_train, target_test, 8)
     
 
-print("\n\n\n")
-print("Accuracy with scaling:\n","--------------------\n")
+# print("\n\n\n")
+# print("Accuracy with scaling:\n","--------------------\n")
 
-scaler = StandardScaler()
-# scaler = RobustScaler()
-# scaler = MinMaxScaler()
-scaled_train_data = scaler.fit_transform(data_train, target_train)
-scaled_test_data = scaler.fit_transform(data_test, target_test)
-log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
-MLHelper.FitAndGetAccuracy(log_reg_model, scaled_train_data, scaled_test_data, \
-                            target_train, target_test, 8)
+# scaler = StandardScaler()
+# # scaler = RobustScaler()
+# # scaler = MinMaxScaler()
+# scaled_train_data = scaler.fit_transform(data_train, target_train)
+# scaled_test_data = scaler.fit_transform(data_test, target_test)
+# log_reg_model = LogisticRegression(C=C_val, max_iter=max_iterations)
+# MLHelper.FitAndGetAccuracy(log_reg_model, scaled_train_data, scaled_test_data, \
+#                             target_train, target_test, 8)
     
-print("\n\n\n")
-print("Accuracy with scaling and cross validation:\n","--------------------\n")
+# print("\n\n\n")
+# print("Accuracy with scaling and cross validation:\n","--------------------\n")
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import cross_val_score
+# from sklearn.datasets import load_iris
+# from sklearn.model_selection import cross_val_score
 
-data = load_iris()
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(data['data'], data['target'])
-cvs = cross_val_score(log_reg_model, scaled_data, data["target"], scoring="accuracy")
+# data = load_iris()
+# scaler = StandardScaler()
+# scaled_data = scaler.fit_transform(data['data'], data['target'])
+# cvs = cross_val_score(log_reg_model, scaled_data, data["target"], scoring="accuracy")
     
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
 
-# Scaling using a Pipeline
-from sklearn.pipeline import Pipeline
-from sklearn.pipeline import make_pipeline
-print("\n\n\n")
-print("Accuracy with Scaling and Using Pipelines:")
+# # Scaling using a Pipeline
+# from sklearn.pipeline import Pipeline
+# from sklearn.pipeline import make_pipeline
+# print("\n\n\n")
+# print("Accuracy with Scaling and Using Pipelines:")
 
-pipeline_w_scaler = Pipeline([
-    ("scalar", StandardScaler()),
-    ("log_reg", LogisticRegression(C=C_val, max_iter=max_iterations))
-    ])
-MLHelper.FitAndGetAccuracy(pipeline_w_scaler, data_train, data_test, \
-                            target_train, target_test, 8)
-cvs = cross_val_score(pipeline_w_scaler, data["data"], data["target"], scoring="accuracy")
-print("Cross Validation Scores: ", cvs)
-print("Cross Validation Mean: ", cvs.mean())
-print("Cross Validation Standard Deviation: ", cvs.std())
+# pipeline_w_scaler = Pipeline([
+#     ("scalar", StandardScaler()),
+#     ("log_reg", LogisticRegression(C=C_val, max_iter=max_iterations))
+#     ])
+# MLHelper.FitAndGetAccuracy(pipeline_w_scaler, data_train, data_test, \
+#                             target_train, target_test, 8)
+# cvs = cross_val_score(pipeline_w_scaler, data["data"], data["target"], scoring="accuracy")
+# print("Cross Validation Scores: ", cvs)
+# print("Cross Validation Mean: ", cvs.mean())
+# print("Cross Validation Standard Deviation: ", cvs.std())
 
 
     
@@ -809,7 +840,73 @@ print("Cross Validation Standard Deviation: ", cvs.std())
 #                                                                             #
 ###############################################################################
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, LabelBinarizer
 
+# Load data that has categpry-based values
+data_file_path = "../../In-Class Exercises/Data/housing.csv"
+data_set = pd.read_csv(data_file_path)
+
+le = LabelEncoder()
+encoded_ocean_data = le.fit_transform(data_set['ocean_proximity'])
+
+# Remove the string-based data and put in the encoded data
+data_encoded = data_set.drop('ocean_proximity', axis=1)
+data_encoded['ocean_proximity'] = encoded_ocean_data
+print(data_encoded.head(10))
+
+
+
+
+
+# One-hot encoding the data
+# This will build off of the numerically-categorized data we produced above
+ohe = OneHotEncoder()
+
+# The fit_transform method expects a 2D matrix, so we need to convert the 
+# encoded data from a 1D array to a 2D array.
+encoded_ocean_data_reshaped = encoded_ocean_data.reshape(-1,1)
+ohe_ocean_data = ohe.fit_transform(encoded_ocean_data_reshaped)
+
+# By default, one-hot encoding returns a SciPy sparse matrix to save memory.
+# To add this to our dataframe, we can convert it to a normal array.
+ohe_ocean_data_arr = ohe_ocean_data.toarray()
+
+# If we transpose the matrix, the data will be in a format easier to make
+# new columns with for our Pandas dataframe
+ohe_ocean_data_arr = ohe_ocean_data_arr.transpose()
+
+# Now we can make a column for each category name.  To check what each category
+# number corresponds to, we can use the "classes_" property.  We can also
+# use each label directly, as we will to later.
+print("Label categories:", le.classes_)
+
+# As before, we can remove the old string-category column
+data_ohe_encoded = data_set.drop('ocean_proximity', axis=1)
+
+# Now we can add the new columns for each category, and add the appropriate
+# data.
+
+for category_index in range(len(le.classes_)):
+    category_name = le.classes_[category_index]
+    print("Category to add to the dataframe: ", category_name)
+    data_ohe_encoded[category_name] = ohe_ocean_data_arr[category_index]
+    
+print(data_ohe_encoded.head(1))
+
+
+
+
+
+# We can use the LabelBinarizer to accomplish the label encoding, and the 
+# one-hot encoding at the same time.  This will produce separate arrays
+# that we will still need to manipulate into a form that a Pandas dataframe
+# will like.
+lb = LabelBinarizer()
+ocean_cat_lb = lb.fit_transform(data_set['ocean_proximity'])
+
+# We can check if this is the same as our previous output after applying the
+# label encoding and one-hot encoding sequentially
+print("LabelBinarizer Check:", ocean_cat_lb == ohe_ocean_data)
 
 
 ###############################################################################
@@ -818,6 +915,7 @@ print("Cross Validation Standard Deviation: ", cvs.std())
 #                                                                             #
 ###############################################################################
 
+#from sklearn.preprocessing import Imputer
 
 
 

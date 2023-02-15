29316addc21895f6c7e27e58cdbdc114ca48e3d0
Douglas Bowman <dbowmans46@gmail.com>
2023-02-14 23:26:39 -0500

    Updated slides for manual engineered features, updated slides for scikit-learn built-in data sets, code to show types of built-in data sets and examples to plot them.

--- Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
+++ Session 7 - Python/Instructional Material/Class Examples/Scikit-Learn/Machine Learning with Scikit-Learn.py
@@ -1352,55 +1352,55 @@ def GetHousingData():
 
 ###############################################################################
 #                                                                             #
-#          Dimensionality Reduction - Locally Linear Embedding on S           #
+#       Dimensionality Reduction - Locally Linear Embedding on S Curve        #
 #                                                                             #
 ###############################################################################
 
-from sklearn.datasets import make_s_curve
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d
+# from sklearn.datasets import make_s_curve
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# import mpl_toolkits.mplot3d
 
 
-# Code modified from SkLearn's website, https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html#sphx-glr-auto-examples-manifold-plot-compare-methods-py
-# and Hands-On ML GitHub page: https://github.com/ageron/handson-ml2/blob/master/08_dimensionality_reduction.ipynb
-points, color = make_s_curve(n_samples=1000, noise=0.2, random_state=0)
+# # Code modified from SkLearn's website, https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html#sphx-glr-auto-examples-manifold-plot-compare-methods-py
+# # and Hands-On ML GitHub page: https://github.com/ageron/handson-ml2/blob/master/08_dimensionality_reduction.ipynb
+# points, color = make_s_curve(n_samples=1000, noise=0, random_state=0)
 
-x, y, z = points.T
+# x, y, z = points.T
 
-fig, ax = plt.subplots(
-    figsize=(6, 6),
-    facecolor="white",
-    tight_layout=True,
-    subplot_kw={"projection": "3d"},
-)
-fig.suptitle("S-Curve", size=16)
-col = ax.scatter(x, y, z, c=color, s=50, alpha=0.8)
-ax.view_init(azim=-60, elev=9)
-ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
-ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
-ax.zaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# fig, ax = plt.subplots(
+#     figsize=(6, 6),
+#     facecolor="white",
+#     tight_layout=True,
+#     subplot_kw={"projection": "3d"},
+# )
+# fig.suptitle("S-Curve", size=16)
+# col = ax.scatter(x, y, z, c=color, s=50, alpha=0.8)
+# ax.view_init(azim=-60, elev=9)
+# ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# ax.zaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
 
-fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
-plt.show()
+# fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
+# plt.show()
 
 
-from sklearn.manifold import LocallyLinearEmbedding
+# from sklearn.manifold import LocallyLinearEmbedding
 
-lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
-X_reduced = lle.fit_transform(points)
+# lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
+# X_reduced = lle.fit_transform(points)
 
-# Code to generate Figure 8–12. Unrolled Swiss roll using LLE:
+# # Code to generate Figure 8–12. Unrolled Swiss roll using LLE:
 
-plt.title("Unrolled S curve using LLE", fontsize=14)
-plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
-plt.xlabel("$z_1$", fontsize=18)
-plt.ylabel("$z_2$", fontsize=18)
-plt.axis([-0.065, 0.055, -0.1, 0.12])
-plt.grid(True)
+# plt.title("Unrolled S curve using LLE", fontsize=14)
+# plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
+# plt.xlabel("$z_1$", fontsize=18)
+# plt.ylabel("$z_2$", fontsize=18)
+# plt.axis([-0.065, 0.055, -0.1, 0.12])
+# plt.grid(True)
 
-#save_fig("lle_unrolling_plot")
-plt.show()
+# #save_fig("lle_unrolling_plot")
+# plt.show()
 
 
 
@@ -1410,51 +1410,51 @@ plt.show()
 #                                                                             #
 ###############################################################################
 
-from sklearn.datasets import make_swiss_roll
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d
+# from sklearn.datasets import make_swiss_roll
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# import mpl_toolkits.mplot3d
 
 
-# Code modified from SkLearn's website, https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html#sphx-glr-auto-examples-manifold-plot-compare-methods-py
-# and Hands-On ML GitHub page: https://github.com/ageron/handson-ml2/blob/master/08_dimensionality_reduction.ipynb
-points, color = make_swiss_roll(n_samples=1000, noise=0.2, random_state=0)
+# # Code modified from SkLearn's website, https://scikit-learn.org/stable/auto_examples/manifold/plot_compare_methods.html#sphx-glr-auto-examples-manifold-plot-compare-methods-py
+# # and Hands-On ML GitHub page: https://github.com/ageron/handson-ml2/blob/master/08_dimensionality_reduction.ipynb
+# points, color = make_swiss_roll(n_samples=1000, noise=0, random_state=0)
 
-x, y, z = points.T
+# x, y, z = points.T
 
-fig, ax = plt.subplots(
-    figsize=(6, 6),
-    facecolor="white",
-    tight_layout=True,
-    subplot_kw={"projection": "3d"},
-)
-fig.suptitle("Swiss Roll", size=16)
-col = ax.scatter(x, y, z, c=color, s=50, alpha=0.8)
-ax.view_init(azim=-60, elev=9)
-ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
-ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
-ax.zaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# fig, ax = plt.subplots(
+#     figsize=(6, 6),
+#     facecolor="white",
+#     tight_layout=True,
+#     subplot_kw={"projection": "3d"},
+# )
+# fig.suptitle("Swiss Roll", size=16)
+# col = ax.scatter(x, y, z, c=color, s=50, alpha=0.8)
+# ax.view_init(azim=-60, elev=9)
+# ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
+# ax.zaxis.set_major_locator(mpl.ticker.MultipleLocator(1))
 
-fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
-plt.show()
+# fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
+# plt.show()
 
 
-from sklearn.manifold import LocallyLinearEmbedding
+# from sklearn.manifold import LocallyLinearEmbedding
 
-lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
-X_reduced = lle.fit_transform(points)
+# lle = LocallyLinearEmbedding(n_components=2, n_neighbors=10, random_state=42)
+# X_reduced = lle.fit_transform(points)
 
-# Code to generate Figure 8–12. Unrolled Swiss roll using LLE:
+# # Code to generate Figure 8–12. Unrolled Swiss roll using LLE:
 
-plt.title("Unrolled S curve using LLE", fontsize=14)
-plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
-plt.xlabel("$z_1$", fontsize=18)
-plt.ylabel("$z_2$", fontsize=18)
-plt.axis([-0.065, 0.055, -0.1, 0.12])
-plt.grid(True)
+# plt.title("Unrolled S curve using LLE", fontsize=14)
+# plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=color)
+# plt.xlabel("$z_1$", fontsize=18)
+# plt.ylabel("$z_2$", fontsize=18)
+# plt.axis([-0.065, 0.055, -0.1, 0.12])
+# plt.grid(True)
 
-#save_fig("lle_unrolling_plot")
-plt.show()
+# #save_fig("lle_unrolling_plot")
+# plt.show()
 
 
 
@@ -1500,14 +1500,102 @@ plt.show()
 
 ###############################################################################
 #                                                                             #
-#                             Dataset Collection                              #
+#          Dataset Collection - Using Built-in Datasets from Sklearn          #
 #                                                                             #
 ###############################################################################
 
+# Data wrangling tools
+import pandas as pd
+
+# Plotting tools
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d
+
+
+# Fetch example
+# The data is used to attempt to categorize topics of messages.
+from sklearn.datasets import fetch_20newsgroups
+newsgroup_data = fetch_20newsgroups()
+
+# For this example, we will create a historgram of the count of each type of
+# target.  First, wrangle data and get counts.
+newsgroup_df = pd.DataFrame(data = newsgroup_data.target, columns=["Target"])
+newsgroup_df["Target Name"] = newsgroup_df.apply(lambda row: newsgroup_data.target_names[row[0]], axis=1)
+grouped_data = newsgroup_df.groupby("Target Name",axis=0).count()
+
+# Make the histogram
+plt.bar(grouped_data.index, grouped_data['Target'].values, width=0.8, align='center')
+plt.title("Count of News Piece Topics in Scikit-Learn fetch_20newsgroups")
+plt.xticks(rotation=310, ha='left')
 
 
 
 
+# # Load Example
+# from sklearn.datasets import load_iris
+# iris_dataset = load_iris()
+
+# # For this example, we will try and print 5 dimensions on a 3D scatter plot
+# # First, data wrangle
+# feature_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+# iris_df = pd.DataFrame(iris_dataset.data, columns=feature_names)
+# iris_df['Targets'] = iris_dataset.target
+
+# Setup the 3D plot.
+# fig, ax = plt.subplots(
+#     figsize=(100,100),
+#     facecolor="white",
+#     tight_layout=True,
+#     subplot_kw={"projection": "3d"},
+# )
+
+# standard_text_size = 150
+# fig.suptitle("Iris Data", size=200)
+# # Plot each data point, separating out each classification with its own 
+# # data point marker
+# plot_markers = ['o', '^', 's']
+# for target, data in iris_df.groupby('Targets'):
+#     test = ax.scatter(data['sepal_length'], data['sepal_width'], \
+#                 data['petal_length'], c=data['petal_width'], \
+#                 marker=plot_markers[target], label=plot_markers[target], s=10000)
+# plt.legend(iris_dataset.target_names, prop={'size': standard_text_size})
+
+# # Make the view a little easier to see everything
+# ax.view_init(azim=-45, elev=30)
+
+# cbar = fig.colorbar(test, ax=ax, orientation="horizontal", shrink=0.8, aspect=60, pad=0.01)
+# cbar.ax.tick_params(labelsize=standard_text_size)
+# cbar.ax.set_xlabel('Petal Width (cm)', size=standard_text_size)
+
+# ax.set_xlabel('Sepal Length (cm)', size=standard_text_size)
+# ax.set_ylabel('Sepal Width (cm)', size=standard_text_size)
+# ax.set_zlabel('Petal Length (cm)', size=standard_text_size)
+# plt.show()
+
+
+
+
+# # Make example
+# # We have already seen this one from the LLE example.  This time, we will
+# # make the points finer.
+# from sklearn.datasets import make_swiss_roll
+
+# points, color = make_swiss_roll(n_samples=200000, noise=0, random_state=0)
+# x, y, z = points.T
+
+# fig, ax = plt.subplots(
+#     figsize=(100, 100),
+#     facecolor="white",
+#     tight_layout=True,
+#     subplot_kw={"projection": "3d"},
+# )
+
+# fig.suptitle("Swiss Roll", size=16)
+# col = ax.scatter(x, y, z, c=color, s=50, alpha=0.8)
+# ax.view_init(azim=-60, elev=9)
+# fig.colorbar(col, ax=ax, orientation="horizontal", shrink=0.6, aspect=60, pad=0.01)
+# plt.show()
 
 
 

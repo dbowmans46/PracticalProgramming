c1f17c4f25104535696ef835cdd5dec7b1a935a8
Douglas Bowman <dbowmans46@gmail.com>
2024-10-15 20:49:53 -0400

    Reorganized and cleaned up.  Added more explicit example for viewing raw text.

--- Session 7 - Python/Instructional Material/Class Examples/NLTK/Introduction.py
+++ Session 7 - Python/Instructional Material/Class Examples/NLTK/Introduction.py
@@ -20,33 +20,17 @@ import nltk
 nltk.download()
 
 # TODO: Import all the included texts using from nltk.book import *
-# TODO: Import other texts, such as nltk.corpus.pros_cons
+# TODO: Import other texts, such as nltk.corpus.pros_cons, view with nltk.corpus.pros_cons.raw()
 # TODO: Look at texts that have part-of-speech tags, such as nltk.corpus.brown.tagged_words()
 # TODO: use text1.concordance("monstrous") to show words with context
 # TODO: Get the concordance of other words
 # TODO: Calculate statistical values of text
 # TODO: Count of word: text1.count(<word>)
 # TODO: Frequency distribution of words/symbols: nltk.probability.FreqDist(<text_to_analyze>) or <text_to_analyze>.vocab()
+# TODO: Frequency distribution of words nltk.FreqDist(text1)
 # TODO: Get similar words with text1.similar("monstrous")
 # TODO: text3.generate() to generate text based on a corpus
-# TODO: Frequency distribution of words nltk.FreqDist(text1)
 
-# Context
-
-####
-# Text Preparation
-####
-
-# Stopwords: nltk.corpus.stopwords.words('english')
-# As is alluded to, stopwords for multiple languages are included
-#nltk.corpus.stopwords.words('russian')
-#nltk.corpus.stopwords.words('hinglish')
-# Removing stop words
-#meaningful_words = [moby_dick_words for moby_dick_words in text1.tokens if not lower(moby_dick_words) in nltk.corpus.stopwords.words('english')]
-
-# Removing punctuation and articles
-#meaningful_words = [no_punc_words for no_punc_words in meaningful_words if len(no_punc_words) > 2]
-#nltk.FreqDist(meaningful_words)
 
 
 # Distances between words

6d8ed72abe192d2815607c01b60384db1ce2f179
Douglas Bowman <dbowmans46@gmail.com>
2024-12-17 20:39:36 -0500

    Added worked solutions to stemmer in-class problems.  Clarified description slides of lemmatization.  Added 2 in-class problems for lemmatization.

--- Session 7 - Python/Instructional Material/Class Examples/NLTK/Text Preparation.py
+++ Session 7 - Python/Instructional Material/Class Examples/NLTK/Text Preparation.py
@@ -126,35 +126,101 @@ import nltk
 ###############################################################################
 
 
-# Create a stemmer and use on some test text
-example_text = "He is studying way too hard on maliciously and unnecessarily assigned reading assignments from uncaring professors."
-tokenized_text = nltk.word_tokenize(example_text)
+# # Create a stemmer and use on some test text
+# example_text = "He is studying way too hard on maliciously and unnecessarily assigned reading assignments from uncaring professors."
+# tokenized_text = nltk.word_tokenize(example_text)
+# porterStemmer = nltk.stem.PorterStemmer()
+# for token in tokenized_text:
+#     print("Token:", token, ", Stem:", porterStemmer.stem(token))
+
+# print("\n")
+
+# # Let's create a side-by-side comparison of the Porter and Lancaster stemmers
+# #raw_text = "He does run the store like a drill seargent."   
+# raw_text = "He studied way too hard on maliciously and unnecessarily assigned reading assignments from uncaring professors." 
+# tokenized_text = nltk.word_tokenize(raw_text)
+# porterStemmer = nltk.stem.PorterStemmer()
+# lancasterStemmer = nltk.stem.LancasterStemmer()
+# porter_stems = []
+# lancaster_stems = []
+# for token in tokenized_text:
+#     porter_stems.append(porterStemmer.stem(token))
+#     lancaster_stems.append(lancasterStemmer.stem(token))
+
+# # Let's put all this in a DataFrame for a clear comparison
+# import pandas as pd
+
+# df = pd.DataFrame([tokenized_text, porter_stems, lancaster_stems])
+# df = df.transpose()
+# df.columns=["Tokens", "Porter", "Lancaster"]
+# print(df)
+    
+# # Notice the more-heavily truncated stems of the lancaster stemmer vs the porter
+# # stemmer.
+
+
+
+# # Finding stemmers and token of the longest word
+# longest_word=""
+# for rows in range(df.shape[0]):
+#     if len(longest_word) < len(df.iloc[rows]['Tokens']):
+#         longest_word = df.iloc[rows]['Tokens']
+
+# print("Token:", longest_word)
+# print("Porter:", porterStemmer.stem(longest_word))
+# print("Lancaster:", lancasterStemmer.stem(longest_word))
+
+
+
+# Finding the same stems of different words
+import pandas as pd
+dracula_filepath = "/home/doug/repos/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Books/Dracula.txt"
+with open(dracula_filepath) as fileHandle:
+    dracula_string = fileHandle.read()
+dracula_tokens = nltk.word_tokenize(dracula_string)
+draculaText = nltk.Text(dracula_tokens)
 porterStemmer = nltk.stem.PorterStemmer()
-for token in tokenized_text:
-    print("Token:", token, ", Stem:", porterStemmer.stem(token))
-
-print("\n")
-
-# Let's create a side-by-side comparison of the Porter and Lancaster stemmers
-raw_text = "He does run the store like a drill seargent."    
-tokenized_text = nltk.word_tokenize(raw_text)
 lancasterStemmer = nltk.stem.LancasterStemmer()
 porter_stems = []
 lancaster_stems = []
-for token in tokenized_text:
+context = []
+index=0
+for token in dracula_tokens:
     porter_stems.append(porterStemmer.stem(token))
     lancaster_stems.append(lancasterStemmer.stem(token))
-
-# Let's put all this in a DataFrame for a clear comparison
-import pandas as pd
-
-df = pd.DataFrame([tokenized_text, porter_stems, lancaster_stems])
-df = df.transpose()
-df.columns=["Tokens", "Porter", "Lancaster"]
-print(df)
     
-# Notice the more-heavily truncated stems of the lancaster stemmer vs the porter
-# stemmer.
+    if index < 5:
+        context.append(" ".join(dracula_tokens[0:index+5]))
+    elif index > len(dracula_tokens) - 5:
+        context.append(" ".join(dracula_tokens[index-5:len(dracula_tokens)]))
+    else:
+        context.append(" ".join(dracula_tokens[index-5:index+5]))
+        
+    index += 1
+
+dracula_df = pd.DataFrame([dracula_tokens, porter_stems, lancaster_stems, context])
+dracula_df = dracula_df.transpose()
+dracula_df.columns=["Tokens", "Porter", "Lancaster", "Context"]
+print(dracula_df)
+
+# Can use this to find some examples that may be useful for finding different
+# words
+dracula_df_mismatch = dracula_df[dracula_df["Porter"] != dracula_df["Lancaster"]]
+
+# Then we can look up all the entries for this in a stemmer to see if there
+# are different tokens
+dracula_df_mad = dracula_df[dracula_df["Lancaster"] == "mad"]
+
+tokens_of_mad = []
+for token_val in dracula_df_mad["Tokens"]:
+    if token_val not in tokens_of_mad:
+        tokens_of_mad.append(token_val)
+
+# If we want to automate this, we could check each stem in one of the Porter and
+# /or Lancaster stemmers.  For each of these stems, find all associated tokens
+# and see if there are different words in this list.
+
+
 
 
 

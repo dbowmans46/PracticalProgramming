396002a378f459f169f68276a1ee4c6dae3158b0
Douglas Bowman <dbowmans46@gmail.com>
2025-01-21 22:52:58 -0500

    Added layout for corpus analysis.  Added slides to clarify bag of words scoring, slides that describe Word2Vec, and slides that describe GloVe.  Noralized styling of slides and added references for word embedding.

--- Session 7 - Python/Instructional Material/Class Examples/NLTK/Corpus Analysis.py
+++ Session 7 - Python/Instructional Material/Class Examples/NLTK/Corpus Analysis.py
@@ -14,24 +14,52 @@ Created on Tue Nov 12 20:44:54 2024
 #                                                                             #
 ###############################################################################
 
+import nltk
+
+dracula_filepath = "/home/doug/repos/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Books/Dracula.txt"
+with open(dracula_filepath) as fileHandle:
+    dracula_string = fileHandle.read()
+dracula_tokens = nltk.word_tokenize(dracula_string)
+draculaText = nltk.Text(dracula_tokens)
+draculaFreqDist = draculaText.vocab()
+
 
 
 
 ###############################################################################
 #                                                                             #
-#                                Bag of Words                                 #
+#                           Bag of Words with Count                           #
 #                                                                             #
 ###############################################################################
 
+import sklearn.feature_extraction.text.CountVectorizer
+
+# TODO: Load in the IMDB data into a dataframe
+# TODO: Get all the words as the initial vocab
+# TODO: Remove stop words from vocab
+# TODO: Remove punctuation and small words from vocab
+# TODO: Utilize the CountVectorizer with the vocab to get counts of the vocab to generate our training set.
+
+
+
+
+###############################################################################
+#                                                                             #
+#                           Bag of Words with TF-IDF                          #
+#                                                                             #
+###############################################################################
+
+# To utilize the TF-IDF scoring metric, we need multiple documents.  So first,
+# we must get a corpus with multiple documents that are related.
+
+import nltk
 import sklearn
 
-sklearn.feature_extraction.text.TfidfTransformer
+import sklearn.feature_extraction.text.TfidfTransformer
+import sklearn.feature_extraction.text.TfidfVectorizer
 
+vectorizer = sklearn.feature_extraction.text.TfidfVectorizer()
+train_vectors = vectorizer.fit_transform(X_train)
+test_vectors = vectorizer.transform(X_test)
+print(train_vectors.shape, test_vectors.shape)
 
-
-
-###############################################################################
-#                                                                             #
-#                         Word Embedding - Word2Vec                           #
-#                                                                             #
-###############################################################################

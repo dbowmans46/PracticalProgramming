126c1fe94f1bf8ecc88d063a5da134acdd628785
Douglas Bowman <dbowmans46@gmail.com>
2025-03-18 22:14:01 -0400

    Added code to demonstrate manual calculation of TF-IDF.  Clarified notes.  Reduced number of review documents to utilize in the spirit of memory conservation.

--- Session 7 - Python/Instructional Material/Class Examples/NLTK/Corpus Analysis.py
+++ Session 7 - Python/Instructional Material/Class Examples/NLTK/Corpus Analysis.py
@@ -40,13 +40,16 @@ Created on Tue Nov 12 20:44:54 2024
 # First, let us read in the files and associate data with each review
 
 import os
-#ratings_root_dir = "/home/doug/repos/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Data/aclImdb_v1/aclImdb/"
-ratings_root_dir = "D:/repo/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Data/aclImdb_v1/aclImdb/"
+ratings_root_dir = "/home/doug/repos/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Data/aclImdb_v1/aclImdb/"
+#ratings_root_dir = "D:/repo/PracticalProgramming/Session 7 - Python/Instructional Material/In-Class Exercises/Data/aclImdb_v1/aclImdb/"
 pos_train_dir = ratings_root_dir + "train/pos/"
 neg_train_dir = ratings_root_dir + "train/neg/"
 
 # This list will contain tuple triples with (text,rating,pos=1/neg=0)
 reviews = []
+
+# TODO: These for loops have the same # of iterations.  Run in paralle to
+# possibly save time.
 for file_name in os.listdir(pos_train_dir):
     full_file_path = pos_train_dir + file_name
     with open(full_file_path, 'r', encoding="UTF-8") as file_handle:
@@ -76,6 +79,11 @@ shuffle(reviews)
 
 import pandas as pd
 reviews_df = pd.DataFrame(data=reviews, columns=["reviews","rating","sentiment"])
+
+# Because of memory constraints, we will limit the number of reviews for this
+# tutorial
+num_docs = 500
+reviews_df = reviews_df.head(num_docs)
 
 
 ###############################################################################
@@ -141,10 +149,13 @@ for review in reviews_df["reviews"]:
     # Make sure to split multi-word tokens so they will match our vocab
     # Also remove erroneous periods.  The other tokens and stop words just 
     # won't find any matches in our DataFrame features, and thus will not need
-    # to be removed here.
-    token_count_dict = {}
+    # to be removed here.  We are going to add an extra column that holds
+    # the total tokens within a document.  This will be needed later, and is
+    # more efficient to calculate here
+    token_count_dict = {"doc_word_count":0}
     review = review.replace("/"," ")
     for review_token in nltk.word_tokenize(review):
+        token_count_dict["doc_word_count"] += 1        
         review_token = review_token.replace(".", "")
         
         if review_token in token_count_dict.keys():
@@ -155,7 +166,8 @@ for review in reviews_df["reviews"]:
     review_vocab_counts.append(token_count_dict)
 
 # Due to memory issues, only use the first 1000/25000 reviews
-train_df = pd.DataFrame(data=review_vocab_counts[:1000], columns=reviews_vocab_clean_split)
+reviews_vocab_clean_split.append("doc_word_count") # Add the document count to our list of vocab
+train_df = pd.DataFrame(data=review_vocab_counts, columns=reviews_vocab_clean_split)
 
 # Where there are no matches, NaN will be placed.  These need to be numbers.
 train_df.fillna(0, inplace=True)
@@ -163,18 +175,18 @@ train_df.fillna(0, inplace=True)
 # Taking a look at the train_df, we can see that this is a pretty sparse matrix
 # Convert to a sparse DataFrame to save some memory.  This will not store any 
 # 0 values in the DataFrame in such a way that they can be repopulated later
-train_sparse_df = train_df.astype(pd.SparseDtype("int", 0))
-del train_df
+# train_sparse_df = train_df.astype(pd.SparseDtype("int", 0))
+# del train_df
 
-# We can check the compression as below
-print("Sparse DF Density:", train_sparse_df.sparse.density)
+# # We can check the compression as below
+# print("Sparse DF Density:", train_sparse_df.sparse.density)
 
 # Let's test to see if it was filled in correctly by checking the words in
 # the first review against the columns of those tokens.
-for token in review_vocab_counts[0]:
-    review_vocab_count_val = review_vocab_counts[0][token]
-    train_df_value = train_sparse_df.loc[0][token]
-    print("token:", token, "   review token count:", review_vocab_count_val, "   train_df count:", train_df_value)
+# for token in review_vocab_counts[0]:
+#     review_vocab_count_val = review_vocab_counts[0][token]
+#     train_df_value = train_sparse_df.loc[0][token]
+#     print("token:", token, "   review token count:", review_vocab_count_val, "   train_df count:", train_df_value)
 
 # Looks like we get a key error.  Why might this be?  Hint: check the stop words
 # Note that this isn't necessarily an error, since some tokens have been removed.
@@ -182,18 +194,18 @@ for token in review_vocab_counts[0]:
 # simply check the values sequentially.  We can also only check words that
 # should be in our feature set by not checking stop words and not checking
 # punctuation.
-for token in review_vocab_counts[0]:
-    if len(token) > 1 and token not in nltk.corpus.stopwords.words('english'):
-        review_vocab_count_val = review_vocab_counts[0][token]
-        train_df_value = train_sparse_df.loc[0][token]
-        print("token:", token, "   review token count:", review_vocab_count_val, "   train_df count:", train_df_value)
+# for token in review_vocab_counts[0]:
+#     if len(token) > 1 and token not in nltk.corpus.stopwords.words('english'):
+#         review_vocab_count_val = review_vocab_counts[0][token]
+#         train_df_value = train_sparse_df.loc[0][token]
+#         print("token:", token, "   review token count:", review_vocab_count_val, "   train_df count:", train_df_value)
 
 
 # # Sure is a lot of work.  Wouldn't it be nice if there was a library that just
 # # did this for us?
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer()
-tokenized_data = vectorizer.fit_transform(reviews_df["reviews"])
+# from sklearn.feature_extraction.text import CountVectorizer
+# vectorizer = CountVectorizer()
+# tokenized_data = vectorizer.fit_transform(reviews_df["reviews"])
 
 # Done, noting that this is a sparse matrix numpy data structure
 
@@ -203,13 +215,57 @@ tokenized_data = vectorizer.fit_transform(reviews_df["reviews"])
 #                                                                             #
 ###############################################################################
 
-# To utilize the TF-IDF scoring metric, we need multiple documents.  So first,
-# we must get a corpus with multiple documents that are related.
+# We will build off our current data set since we need the count of tokens
+# to calculate the TF-IDF
 
 # import nltk
 # import sklearn
 
-# from sklearn.feature_extraction.text import TfidfTransformer
+# First, we will calculate the value manually. 
+# TODO: Get the TF value by getting the count of the token over the count of all
+# tokens in this documents.  We will update the count values in place.
+def generate_tf_vals(row, count_col_name = "doc_word_count"):
+    for column in row.keys():
+        if column != count_col_name:
+            row[column] = row[column]/row[count_col_name]
+    return row
+    
+train_df = train_df.apply(generate_tf_vals, axis=1)
+
+ 
+
+# TODO: Get the number of documents this token exists in and calculate IDF val
+# as ln (docs/docs where word appears).  We will create
+# a dictionary for this to make the mapping easier.
+import math
+token_idf_vals = {}
+for token in train_df.columns:
+    count_of_docs_with_token = (train_df[token] != 0.0).sum()
+    # In the math library, log is the natural log, ln
+    token_idf_vals[token] = math.log(num_docs/count_of_docs_with_token)
+    
+# Just as above, we will apply a map to update the current TF values.  We could
+# also simplify by making 1 function that generates the tf-idf value in one go.
+def generate_tf_idf_vals(row_with_tf_vals, token_idf_vals, count_col_name = "doc_word_count"):
+    for column in row_with_tf_vals.keys():
+        if column != count_col_name:
+            row_with_tf_vals[column] = row_with_tf_vals[column]*token_idf_vals[column]
+    return row_with_tf_vals
+
+# TODO: Apply the function
+train_df = train_df.apply(lambda row: generate_tf_idf_vals(row, token_idf_vals), axis=1)
+
+# At this point, we now have a data frame that we can use to train a machine
+# learning model of our choice.
+
+
+# Just as with a simple count, there are already methods that can do this for
+# us.
+# sklearn.feature_extraction.text.TfidfTransformer can be utilized if you
+# already have a matrix of token counts.
+# sklearn.feature_extraction.text.TfidfVectorizer can be used if you are starting
+# with raw documents, such as is the case with our example here.
+
 # from sklearn.feature_extraction.text import TfidfVectorizer
 
 # # Create TF-IDF features
@@ -221,3 +277,9 @@ tokenized_data = vectorizer.fit_transform(reviews_df["reviews"])
 # test_vectors = vectorizer.transform(X_test)
 # print(train_vectors.shape, test_vectors.shape)
 
+
+
+
+# So, if there are methods that already do this for us, why go through all the 
+# hassle of doing it manually?  Sometimes, you need to customize the weighting
+# or the algorithm to better represent the corpus you are working with.
